a4055ba09576a8b7ca35c079ecd9a3f427b1497f
Albert Le <albertle@alberts-mbp.blr.can.ibm.com>
2016-11-10 10:19:06 -0500

    creating user now gives response

--- Points.go
+++ Points.go
@@ -179,7 +179,7 @@ func (t *SimpleChaincode) createAccount(stub *shim.ChaincodeStub, args []string)
 
                 if err == nil {
                     fmt.Println("created account" + account.ID)
-                    return nil, nil
+                    return accountBytes, nil
                 } else {
                     fmt.Println("failed to create initialize account for " + account.ID)
                     return nil, errors.New("failed to initialize an account for " + account.ID + " => " + err.Error())
@@ -197,7 +197,7 @@ func (t *SimpleChaincode) createAccount(stub *shim.ChaincodeStub, args []string)
 
         if err == nil {
             fmt.Println("created account" + account.ID)
-            return nil, nil
+            return accountBytes, nil
         } else {
             fmt.Println("failed to create initialize account for " + account.ID)
             return nil, errors.New("failed to initialize an account for " + account.ID + " => " + err.Error())
